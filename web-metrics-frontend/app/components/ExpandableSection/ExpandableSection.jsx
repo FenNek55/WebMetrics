63cbcbdfaebfcb3f3bfbe25f38ab72d654360b35
Karol Stasik <stasik.karol@gmail.com>
2023-09-16 14:45:04 +0200

    Ad expendable sections to test results

---
+++ web-metrics-frontend/app/components/ExpandableSection/ExpandableSection.jsx
@@ -0,0 +1,21 @@
+import styles from './ExpandableSection.module.css'
+import { useState } from 'react'
+
+const ExpandableSection = ({children, title}) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+    
+    const handleButtonClick = () => {
+        setIsExpanded((prevState) => !prevState)
+    }
+
+    return (
+        <section className={`${styles['expandable-section']} ${isExpanded ? styles['expandable-section--open'] : ''}`}>
+            <button onClick={handleButtonClick} className={styles['expandable-section__button']}><h2>{title}</h2></button>
+            <div className={styles['expandable-section__content']}>
+                {children}
+            </div>
+        </section>
+    )
+}
+
+export default ExpandableSection
